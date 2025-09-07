17406692b000e9937d7add8211b68b98137c1210
amirhosseinaarabi2002 <amiraarabisw2002@gmail.com>
2025-09-07 05:36:17 -0700

    finish untill ban user model

--- backend/controllers/v1/auth.js
+++ backend/controllers/v1/auth.js
@@ -1,11 +1,13 @@
 const userModel = require("../../models/user");
+const bcrypt = require("bcrypt");
+const jwt = require("jsonwebtoken");
 
 const registerValidator = require("../../validators/register");
 
 exports.register = async (req, res) => {
   const validationResult = registerValidator(req.body);
 
-  if (validationResult != true) {
+  if (validationResult !== true) {
     return res.status(422).json(validationResult);
   }
 
@@ -15,11 +17,36 @@ exports.register = async (req, res) => {
     $or: [{ username }, { email }],
   });
 
-  if(isUserExist){
+  if (isUserExist) {
     return res.status(409).json({
-        message: "username or email is duplicated!"
-    })
+      message: "username or email is duplicated!",
+    });
   }
+
+  const countOfUsers = await userModel.countDocuments();
+
+  const hashedPassword = await bcrypt.hash(password, 10);
+
+  const user = await userModel.create({
+    name,
+    username,
+    email,
+    phone,
+    password: hashedPassword,
+    role: countOfUsers > 0 ? "USER" : "ADMIN",
+  });
+
+  const userObject = user.toObject();
+  Reflect.deleteProperty(userObject, "password");
+
+  const accessToken = jwt.sign({ id: user._id }, process.env.JWT_SECRET, {
+    expiresIn: "30 day",
+  });
+
+  return res.status(201).json({
+    user: userObject,
+    accessToken,
+  });
 };
 
 exports.login = async (req, res) => {};
